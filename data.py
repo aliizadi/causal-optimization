9eab062366c5e6de8219872b248e254c59068977
aliizadi <aliizadi2030@gmail.com>
2022-06-13 22:54:03 +0430

    data generation based on causal model

---
+++ data.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+seed = 0
+np.random.seed(seed)
+
+causal_func_dict = {'linear': lambda x, n: 2 * x + n,
+                    'non-linear': lambda x, n: x + (.5) * x * x * x + (n),
+                    # 'nueralnet_l1': lambda x, n: sigmoid(sigmoid(np.random.normal(loc=1) * x) + n),
+                    # 'mnm': lambda x, n: sigmoid(np.random.normal(loc=1) * x) + .5 * x ** 2
+                    #                     + sigmoid(np.random.normal(loc=1) * x) * n
+                    }
+
+
+def generate_bivariate_data(n_samples=100, direction='xy', causal_func='non-linear', noise_dist='gaussian'):
+    if noise_dist == 'laplace':
+        N = np.random.laplace(loc=0, scale=1. / np.sqrt(2), size=(n_samples, 2))
+    elif noise_dist == 'gaussian':
+        N = np.random.normal(loc=0, scale=1., size=(n_samples, 2))
+    elif noise_dist == 'cauchy':
+        N = np.random.standard_cauchy(size=(n_samples, 2))
+    elif noise_dist == 'student':
+        N = np.random.standard_t(df=5, size=(n_samples, 2))
+    else:
+        raise ValueError(noise_dist)
+
+    N_laplace = np.random.laplace(loc=0, scale=1. / np.sqrt(2), size=(n_samples, 1))
+    N_gaussian = np.random.normal(loc=0, scale=1., size=(n_samples, 1))
+
+    X = np.zeros((n_samples, 2))
+    X[:, 0] = N_gaussian[:, 0]
+    X[:, 1] = causal_func_dict[causal_func](X[:, 0], N_laplace[:, 0])
+
+    if direction == 'yx':
+        X = X[:, [1, 0]]
+        print(f'direction: {direction}')
+    elif direction == 'xy':
+        print(f'direction: {direction}')
+        pass
+
+    return X
+
+
+def generate_multivariate_data(n_samples=100, direction='xy', causal_func='non-linear', noise_dist='gaussian'):
+    dim = 4
+    if noise_dist == 'laplace':
+        N = np.random.laplace(loc=0, scale=1. / np.sqrt(2), size=(n_samples, dim))
+    elif noise_dist == 'gaussian':
+        N = np.random.normal(loc=0, scale=1., size=(n_samples, dim))
+    elif noise_dist == 'cauchy':
+        N = np.random.standard_cauchy(size=(n_samples, dim))
+    elif noise_dist == 'student':
+        N = np.random.standard_t(df=5, size=(n_samples, dim))
+    else:
+        raise ValueError(noise_dist)
+
+    X = np.zeros((n_samples, dim))
+    X[:, 0] = N[:, 0]
+    X[:, 1] = causal_func_dict[causal_func](X[:, 0], N[:, 1])
+    X[:, 2] = causal_func_dict[causal_func](X[:, 0], N[:, 2])
+    # X[:, 1] = N[:, 1]
+    # X[:, 2] = N[:, 2]
+
+    X[:, 3] = causal_func_dict[causal_func](X[:, 1] + X[:, 2], N[:, 3])
+
+    return X
