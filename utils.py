ad8cd362e69b690d405b38be012ca3d8f9bd436c
aliizadi <aliizadi2030@gmail.com>
2022-06-20 11:06:49 +0430

    move hungarian algorithm to utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+from scipy.optimize import linear_sum_assignment
+from scipy.sparse import csr_matrix
+
+
+def hungarian_algorithm(grad):
+    """
+    https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.optimize.linear_sum_assignment.html
+    """
+    row_ind, col_ind = linear_sum_assignment(grad)
+    shape = len(row_ind)
+    data = [1 for _ in range(shape)]
+    return csr_matrix((data, (row_ind, col_ind)), shape=(shape, shape)).toarray()
